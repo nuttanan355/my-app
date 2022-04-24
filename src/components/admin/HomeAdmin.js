509b4d06b8a49f70d5ccda7f82e4d56efab11e46
Wichakool <88084670+Wichakool@users.noreply.github.com>
2022-04-25 05:32:00 +0700

    5.31 25/4/2022 tong

--- src/components/admin/HomeAdmin.js
+++ src/components/admin/HomeAdmin.js
@@ -3,80 +3,48 @@ import React from "react";
 // import { Container, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import NavbarAdmin from "../../navigation/navbar_admins";
-// import * as FaIcons from "react-icons/fa";
-// import * as AiIcons from "react-icons/ai";
+import * as AiIcons from "react-icons/ai";
 import * as FcIcons from "react-icons/fc";
-
+import * as FiIcons from "react-icons/fa";
+import * as RiIcons from "react-icons/ri";
+import * as BsIcons from "react-icons/bs";
+import * as HiIcons from "react-icons/hi";
 function HomeAdmin() {
   return (
-    <div>
-      {/* <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="/HomeAdmin">
-        <img
-          alt=""
-          src="../../img/logo_kmutnb.png"
-          // width="30"
-          // height="30"
-          className="d-inline-block align-top"
-        />{' '}
-     HomeAdmin
-      </Navbar.Brand>
-    </Container>
-  </Navbar> */}
+    <div style={{ background: "lightgray" }}>
       <NavbarAdmin />
-      <div className="container">
+      <div className="container" >
         <div className="container mt-5">
-          <h1>ADMIN</h1>
+          <h1 style={{ fontWeight: "bold", color: "gray" }}>ADMIN</h1>
         </div>
         <hr />
         <div className="container">
-          <div className="row">
-            <div
-              className="col-lg bg-secondary bg-opacity-10 rounded-3 p-3 mx-3 my-3"
-              id="stateProduct"
-            >
-              <Link to={"/OrdersAdmin"}>
-              <div className="text-center">
-                <FcIcons.FcRatings size={200} />
-              </div>
-              <p className="text-center mt-3 h4">รายการคำสั่งซื้อจากผู้ซื้อ</p>
-              </Link>
-            </div>
-            <div
-              className="col-lg bg-success bg-opacity-10  rounded-3 p-3 mx-3 my-3 text-dark"
-              id="div2"
-            >
-              <Link to={"/NewProductsAdmin"}>
-                <div className="text-center">
-                  <FcIcons.FcComboChart size={200} />
-                </div>
-                <p className="text-center mt-3 h4"> ตรวจสอบการอัปโหลด อนุมัติรายการสินค้า </p>
-              </Link>
-            </div>
-            <div
-              className="col-lg bg-secondary bg-opacity-10  rounded-3 p-3 mx-3 my-3"
-              id="div3"
-            >
-              <Link to={'/EditProductAdmin'}>
-              <div className="text-center">
-                <FcIcons.FcDataConfiguration size={200} />
-              </div>
-              <p className="text-center mt-3 h4">ตรวจสอบการแก้ไขรายการสินค้า</p>
-            </Link>
-            </div>
-            <div
-              className="col-lg bg-secondary bg-opacity-10  rounded-3 p-3 mx-3 my-3"
-              id="stateProduct"
-            >
-              <Link to={'/RecomAdmin'}>
-              <div className="text-center">
-                <FcIcons.FcLike size={200} />
-              </div>
-              <p className="text-center mt-3 h4">บริการสินค้าแนะนำ</p>
-              </Link>
-            </div>
+
+          <div className="col">
+            <a href="/OrdersAdmin">
+              <button className="btn btn-outline-secondary" style={{ width: "20%", margin: "2%", border: "0" }}>
+                <RiIcons.RiFileList2Line size={200} />
+                <div style={{ fontSize: "25px" }}>รายการคำสั่งซื้อจากผู้ซื้อ </div>
+              </button>
+            </a>
+            <a href="/NewProductsAdmin">
+              <button className="btn btn-outline-secondary" style={{ width: "20%", margin: "2%", border: "0" }}>
+                <BsIcons.BsCardChecklist size={200} />
+                <div style={{ fontSize: "25px" }}>ตรวจสอบการอัปโหลด</div>
+              </button></a>
+            <a href="/EditProductAdmin">
+              <button className="btn btn-outline-secondary" style={{ width: "20%", margin: "2%", border: "0" }}>
+                <AiIcons.AiOutlineEdit size={200} />
+                <div style={{ fontSize: "25px" }}>ตรวจสอบการแก้ไข</div>
+              </button></a>
+            <a href="/RecomAdmin">
+              <button className="btn btn-outline-secondary" style={{ width: "20%", margin: "2%", border: "0" }}>
+                <RiIcons.RiStarFill size={200} />
+                <div style={{ fontSize: "25px" }}>บริการสินค้าแนะนำ</div>
+              </button>
+            </a>
           </div>
+
         </div>
       </div>
     </div>
--- src/navigation/navbar_admins.js
+++ src/navigation/navbar_admins.js
@@ -1,22 +1,48 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
+import * as FcIcons from "react-icons/fc";
+import * as RiIcons from "react-icons/ri";
+import * as BsIcons from "react-icons/bs";
+import { Link } from "react-router-dom";
 function NavbarAdmin() {
   return (
-    <div>
-      <Navbar bg="#00B952" variant="dark">
-        <Container>
-          <Navbar.Brand href="/HomeAdmin">
-            <img
-              alt=""
-              src="../../img/logo_kmutnb.png"
-              // width="30"
-              // height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            HomeAdmin
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+    <div className="pos-f-t">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-dark p-4">
+          <div>
+            <a className="text-white" style={{ margin: "15px", fontSize: "28px", fontWeight: "bold" }}>Menu Admin</a>
+          </div>
+
+          <hr style={{ color: "black" }} />
+
+          <div className="container" style={{ textAlign: "center" }}>
+            <div className="row" >
+
+              <div className="btn col-lg-6" >
+                |
+                <a className="btn btn-outline-light" href="http://scanfcode.com/category/c-language/" style={{ padding: "10px", margin: "1px 0px 0px 0px", width: "50%", fontSize: "18px", border: "0" }}> รายการคำสั่งซื้อจากผู้ซื้อ</a> |
+                <a className="btn btn-outline-light" href="http://scanfcode.com/category/front-end-development/" style={{ padding: "10px", margin: "1px 0px 0px 0px", width: "50%", fontSize: "18px", border: "0" }}>ตรวจสอบการอัปโหลด</a> |
+                <a className="btn btn-outline-light" href="http://scanfcode.com/category/back-end-development/" style={{ padding: "10px", margin: "1px 0px 0px 0px", width: "50%", fontSize: "18px", border: "0" }}>ตรวจสอบการแก้ไข</a> |
+                <a className="btn btn-outline-light" href="http://scanfcode.com/category/java-programming-language/" style={{ padding: "10px", margin: "1px 0px 0px 0px", width: "50%", fontSize: "18px", border: "0" }}>บริการสินค้าแนะนำ</a>
+                |
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark bg-dark col-md-12">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <div className="navbar-toggler-icon"></div>
+        </button>
+        <Link className="navbar-toggler" to="/HomeAdmin" style={{ border: "0" }}>
+          TEA Marketplance
+        </Link>
+        <div style={{ textAlign: "right" }}>
+          <a className="text-white" style={{ margin: "32px", textAlign: "right", fontSize: "18px" }}>Admin User :</a>
+        </div>
+
+      </nav>
     </div>
   );
 }

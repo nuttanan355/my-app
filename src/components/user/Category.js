6126bc74f8d24b46b0ac7577b875e2c55e619642
Wichakool <88084670+Wichakool@users.noreply.github.com>
2022-05-18 21:56:44 +0700

    T

--- src/components/user/Category.js
+++ src/components/user/Category.js
@@ -20,23 +20,28 @@ function Category() {
   return (
     <div><a href="/"> หน้าหลัก \</a><a href="#"> หมวดหมู่ \</a>
 
-
-      <div className='flexbox' style={{ minHeight: "1000px" }}>
-        {Object.keys(values).map((id, index) => {
-          return (
-            <div className="flex-item" key={index} >
-              <div>
-                <img className='img-item' src={values[id].productImg[0]} />
-                <div className='name-item' style={{ position: "absolute", height: "50px", background: "white", opacity: "50%", width: "300px" }}>
-                  {values[id].productName}
+      <div className='default-bg'>
+        <div className='flexbox' >
+          {Object.keys(values).map((id, index) => {
+            return (
+              <div type="button" className="flex-item" key={index} >
+                <div className='card' >
+                  <img className='img-item' src={values[id].productImg[0]} />
+                  <div className='title-item'>
+                    <div className='name-item'>
+                    {values[id].productName}
+                    </div>
+                    <div className='price-item'>
+                    {values[id].productPrice} ฿
+                    </div>
+                  </div>
                 </div>
+
               </div>
-
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-
 
     </div>
   )

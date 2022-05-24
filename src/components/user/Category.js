e18769d29152855f2b86bec043d6252aec52106c
Nuttanan Jantaramanee <nuttanan355@gmail.com>
2022-05-24 14:03:05 +0700

    (JJ) Update Category

--- src/components/user/Category.js
+++ src/components/user/Category.js
@@ -1,39 +1,75 @@
-import React from 'react'
+import React from "react";
 import { firebaseDB } from "../../server/firebase";
 import { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
-import { Form } from 'react-bootstrap';
-import { GrouprData } from '../../client/GroupData';
+import { Form } from "react-bootstrap";
+import { GrouprData } from "../../client/GroupData";
+import Error007 from "../../error_007";
 
 function Category() {
   const [values, setValues] = useState({});
+  const [categorys, setCategorys] = useState("");
+
   useEffect(() => {
-    firebaseDB.child("Product").orderByChild("productAllow").equalTo(true).once("value", (snapshot) => {
-      if (snapshot.val() !== null) {
-        setValues({ ...snapshot.val() });
-        console.log(snapshot.val());
-      } else {
-        setValues({});
-      }
-    });
+    if (categorys !== "") {
+      firebaseDB
+        .child("Product")
+        .orderByChild("productCategory")
+        .equalTo(categorys)
+        .once("value", (snapshot) => {
+          // productCategory
+          if (snapshot.val() !== null) {
+            setValues({ ...snapshot.val() });
+            console.log(snapshot.val());
+          } else {
+            setValues({});
+          }
+        });
+    }  else {
+      firebaseDB
+        .child("Product")
+        .orderByChild("productAllow")
+        .equalTo(true)
+        .once("value", (snapshot) => {
+          if (snapshot.val() !== null) {
+            setValues({ ...snapshot.val() });
+            console.log(snapshot.val());
+          } else {
+            setValues({});
+          }
+        });
+    }
+
     return () => {
       setValues({});
     };
-  }, []);
+  }, [categorys]);
+
+  console.log(categorys);
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>สินค้าทั้งหมด</h1>
-      <div style={{ padding: "50px", paddingTop: "20px", paddingLeft: "20px", background: "white", border: "1px solid lightgray", borderRadius: "20px" }}>
-        <div className='flexbox' style={{ justifyContent: "center" }} >
+      <div
+        style={{
+          padding: "50px",
+          paddingTop: "20px",
+          paddingLeft: "20px",
+          background: "white",
+          border: "1px solid lightgray",
+          borderRadius: "20px",
+        }}
+      >
+        <div className="flexbox" style={{ justifyContent: "center" }}>
           <div style={{ display: "block", width: "100%" }}>
             <select
-           
               aria-label="Default select example"
               id="productCategory"
               name="productCategory"
               className="form-select"
               required
               style={{ marginBottom: "20px", width: "250px" }}
+              onChange={(e) => setCategorys(e.target.value)}
             >
               <option value="">ประเภทสินค้า</option>
               {GrouprData.map((item, keys) => {
@@ -48,31 +84,78 @@ function Category() {
 
           {Object.keys(values).map((id, index) => {
             return (
-              <div style={{ display: "block ", height: "300px", padding: "5px", width: "12.5%" }} key={index} >
-                <div type="button" className="hoverproduct" onClick={() => (window.location.href = `/view-product/${id}`)} style={{ borderRadius: "5px" }}>
-                  <div className='img-product' >
-                    <img
-                      id="imgShow"
-                      className="card-img-top "
-                      style={{ width: "100%", height: '180px', borderRadius: "5px 5px 0px 0px" }}
-                      alt="Product Images"
-                      src={values[id].productImg[0]}
-                    />
-                  </div>
-
-                  <div style={{ padding: "10px", background: "white", height: "80px", borderRadius: "0px 0px 5px 5px" }}>
-                    <div style={{ height: "40px", overflow: "hidden" }}>
-                      <p className='cut-text-multi' style={{ fontWeight: "bold", fontSize: "12px", textAlign: "left" }}>{values[id].productName}</p>
+              <div
+                style={{
+                  display: "block ",
+                  height: "300px",
+                  padding: "5px",
+                  width: "12.5%",
+                }}
+                key={index}
+              >
+                {values[id].productAllow ? (
+                  <div
+                    type="button"
+                    className="hoverproduct"
+                    onClick={() =>
+                      (window.location.href = `/view-product/${id}`)
+                    }
+                    style={{ borderRadius: "5px" }}
+                  >
+                    <div className="img-product">
+                      <img
+                        id="imgShow"
+                        className="card-img-top "
+                        style={{
+                          width: "100%",
+                          height: "180px",
+                          borderRadius: "5px 5px 0px 0px",
+                        }}
+                        alt="Product Images"
+                        src={values[id].productImg[0]}
+                      />
                     </div>
-                    <p style={{ marginTop: "5px", fontWeight: "bold", fontSize: "12px", textAlign: "left", color: "#14DDA0" }}> {values[id].productPrice} บาท </p>
+
+                    <div
+                      style={{
+                        padding: "10px",
+                        background: "white",
+                        height: "80px",
+                        borderRadius: "0px 0px 5px 5px",
+                      }}
+                    >
+                      <div style={{ height: "40px", overflow: "hidden" }}>
+                        <p
+                          className="cut-text-multi"
+                          style={{
+                            fontWeight: "bold",
+                            fontSize: "12px",
+                            textAlign: "left",
+                          }}
+                        >
+                          {values[id].productName}
+                        </p>
+                      </div>
+                      <p
+                        style={{
+                          marginTop: "5px",
+                          fontWeight: "bold",
+                          fontSize: "12px",
+                          textAlign: "left",
+                          color: "#14DDA0",
+                        }}
+                      >
+                        {values[id].productPrice} บาท
+                      </p>
+                    </div>
                   </div>
-
-
-
-
-                </div>
+                ) : values == null ? (
+                  console.log("NOT PRODUCT")
+                ) : (
+                  // <Error007/>
+                  <></>
+                )}
               </div>
-
 
               // <div type="button" key={index}
               //   style={{
@@ -109,16 +192,13 @@ function Category() {
               // </div>
             );
           })}
-
-
-
         </div>
       </div>
       <br></br>
       <br></br>
       <br></br>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
---
+++ src/error_007.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/error404.css'
+
+function Error007(){
+    return(<div>
+        <div id="notProduct">
+		<div className="notProduct">
+			<div className="notproduct-007"></div>
+			<h1>ไม่พบสินค้า</h1>
+			<h2>กรุณาลองใหม่อีกครั้ง !</h2>
+			
+			<a href="/">Back to homepage</a>
+		</div>
+	</div>
+    </div>);
+}export default Error007;
